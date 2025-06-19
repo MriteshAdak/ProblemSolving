1c95daed3aa44b75cf2ffc807d762c36d6cefc2a
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2025-06-19 09:15:25 +0200

    Solved Remove Element Problem

---
+++ removeElement.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    for (let index = 0; index < nums.length ; index++) {
+        if (nums[index] === val) {
+            nums.splice(index, 1);
+            index--;
+        }
+    }
+    return nums.length;
+};
