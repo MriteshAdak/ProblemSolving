95bc636ec1a3ddd668bae469fc59fce83d88d0f8
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2025-06-26 23:19:41 +0200

    Solved the Plus One problem

---
+++ plusOne.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+    if (digits[digits.length - 1] !== 9) {
+        digits[digits.length - 1]++;
+        return digits;
+    }
+    else {
+        for (let i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] !== 9) {
+                digits[i]++;
+                return digits;
+            }
+            else if (i !== 0) {
+                digits[i] = 0;
+            }
+            else if (i === 0 && digits[i] === 9){
+                digits[i] = 0;
+                digits.unshift(1);
+                return digits;
+            }
+        }
+    }
+};
+
+//Solved
