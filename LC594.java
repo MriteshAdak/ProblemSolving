8dc39a5503873dad413169f3761518c07aeacff4
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2025-08-12 01:28:20 +0200

    Optimized logic for Leetcode problem #594

--- LC594.java
+++ LC594.java
@@ -1,41 +1,14 @@
+
 class Solution {
     public int findLHS(int[] nums) {
-        List<Integer> uniqueValuesInArray = new ArrayList<>();
-        Map<Integer, Integer> mapOfNumbersAndTheirCountInArray = new TreeMap<>();
+        Map<Integer, Integer> frequencyMapping = new TreeMap<>();
+        for (int n : nums) frequencyMapping.put(n, frequencyMapping.getOrDefault(n, 0) + 1);
 
-        for (int n : nums) {
-            if (!uniqueValuesInArray.contains(n)) uniqueValuesInArray.add(n);
-        }
-        uniqueValuesInArray.sort(null);
-        // System.out.println(uniqueValuesInArray);
-
-        if(uniqueValuesInArray.size() <= 2) {
-            // System.out.println("too small");
-            if(uniqueValuesInArray.size() <= 1 || !uniqueValuesInArray.get(1).equals(uniqueValuesInArray.get(0)+1))
-                return 0;
-            else
-                return nums.length;
-        }
-
-        for (Integer n : uniqueValuesInArray) {
-            Integer count = 0;
-            for (int k : nums) if (k == n) count++;
-            mapOfNumbersAndTheirCountInArray.put(n, count);
-        }
-
-        // System.out.println(mapOfNumbersAndTheirCountInArray);
-        
         int max = 0;
-        for (int i = 1; i < uniqueValuesInArray.size(); i++) {
-            // System.out.println(uniqueValuesInArray.get(i).equals(uniqueValuesInArray.get(i-1)+1));
-            // System.out.println(uniqueValuesInArray.get(i));
-            // System.out.println(uniqueValuesInArray.get(i-1)+1);
-            if(uniqueValuesInArray.get(i).equals(uniqueValuesInArray.get(i-1)+1)) {
-                int combinedCount = mapOfNumbersAndTheirCountInArray.get(uniqueValuesInArray.get(i)) + 
-                                    mapOfNumbersAndTheirCountInArray.get(uniqueValuesInArray.get(i-1));
-
-                // System.out.println(combinedCount);
-                if (combinedCount > max) max = combinedCount;
+        for(Integer num : frequencyMapping.keySet()) {
+            if (frequencyMapping.containsKey(num + 1)) {
+                int combinedLength = frequencyMapping.get(num) + frequencyMapping.get(num + 1);
+                max = Math.max(combinedLength, max);
             }
         }
         return max;
